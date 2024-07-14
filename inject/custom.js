a9c56e6d7a9dbe6c8a7f0bac668e268437076903
ramsayi <ramsayi726@gmail.com>
2024-07-14 10:03:25 +0800

    Site updated: 2024-07-14 10:03:21

--- inject/custom.js
+++ inject/custom.js
@@ -22,42 +22,42 @@
   })
 })()
 
-// 点击宽屏
-;(function () {
-  // 获取#rightside-config-show
-  const rightsideConfigShow = document.querySelector('#rightside-config-show')
+// // 点击宽屏
+// ;(function () {
+//   // 获取#rightside-config-show
+//   const rightsideConfigShow = document.querySelector('#rightside-config-show')
 
-  // 创建按钮元素
-  const button = document.createElement('button')
-  button.id = 'show-wide-content'
-  button.textContent = '超宽'
+//   // 创建按钮元素
+//   const button = document.createElement('button')
+//   button.id = 'show-wide-content'
+//   button.textContent = '超宽'
 
-  // 将按钮添加到#rightside-config-show中
-  rightsideConfigShow.appendChild(button)
+//   // 将按钮添加到#rightside-config-show中
+//   rightsideConfigShow.appendChild(button)
 
-  // 获取#show-wide-content
-  const showWideContent = document.querySelector('#show-wide-content')
+//   // 获取#show-wide-content
+//   const showWideContent = document.querySelector('#show-wide-content')
 
-  // 定义点击事件处理函数
-  function showWideContentHandler() {
-    const asideContent = document.querySelector('#aside-content')
-    const contentInner = document.querySelector('#content-inner')
+//   // 定义点击事件处理函数
+//   function showWideContentHandler() {
+//     const asideContent = document.querySelector('#aside-content')
+//     const contentInner = document.querySelector('#content-inner')
 
-    // 设置动画
-    asideContent.style.transition = 'width 1s ease-in-out'
-    contentInner.style.transition = 'max-width 1s ease-in-out'
+//     // 设置动画
+//     asideContent.style.transition = 'width 1s ease-in-out'
+//     contentInner.style.transition = 'max-width 1s ease-in-out'
 
-    if (asideContent.style.width === '600px') {
-      // 恢复默认样式
-      asideContent.style.width = ''
-      contentInner.style.maxWidth = ''
-    } else {
-      // 设置新样式
-      asideContent.style.width = '600px'
-      contentInner.style.maxWidth = '1920px'
-    }
-  }
+//     if (asideContent.style.width === '600px') {
+//       // 恢复默认样式
+//       asideContent.style.width = ''
+//       contentInner.style.maxWidth = ''
+//     } else {
+//       // 设置新样式
+//       asideContent.style.width = '600px'
+//       contentInner.style.maxWidth = '1920px'
+//     }
+//   }
 
-  // 为#show-wide-content添加点击事件监听器
-  showWideContent.addEventListener('click', showWideContentHandler)
-})()
+//   // 为#show-wide-content添加点击事件监听器
+//   showWideContent.addEventListener('click', showWideContentHandler)
+// })()
