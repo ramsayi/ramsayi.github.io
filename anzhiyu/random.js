6b25f2a706e30fe3011b1545b0b3570fb787dd9b
ramsayi <ramsayi726@gmail.com>
2026-02-05 15:15:26 +0800

    Site updated: 2026-02-05 15:15:23

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["posts/9daba997/","posts/70c2fc3d/","posts/280f8d40/","posts/b8f4bd70/","posts/a0b1cc7d/","posts/f0c817ec/","posts/899df239/","posts/f2029d3b/","posts/ad2d1971/","posts/295f256c/","posts/81760b1b/","posts/d33dd9c1/","posts/fc876b88/","posts/b2c596f5/","posts/b2d509e5/","posts/d8b4433f/","posts/e4e49048/","posts/261ddbfa/","posts/3fc4dfb9/","posts/23ff30ca/","posts/7a25a720/","posts/89ef8535/","posts/800690c0/","posts/220267f2/","posts/c34a302d/","posts/b5e6e462/","posts/36da500e/","posts/f9b5041/","posts/f48c336e/","posts/a01a2cba/","posts/20b9268c/","posts/1a350f0a/","posts/4778d367/","posts/8fd81083/","posts/726b46a7/","posts/53d4ee55/","posts/cdb07bf6/","posts/861974c8/","posts/f857272c/","posts/4ba28477/","posts/aa3c288f/","posts/dae7ff7b/","posts/2aae77a2/","posts/739fa8db/","posts/5cfb881b/","posts/3cbedc1f/","posts/fea9501a/","posts/448a4ec5/","posts/d388acaa/","posts/ddd39f3e/","posts/cbc34163/","posts/1761ca15/","posts/f4b888f4/","posts/2bb37b42/","posts/55b2a1a5/","posts/808c487a/","posts/fec652e4/","posts/73424431/","posts/8f29c8ea/","posts/1fd37361/","posts/306ffe3/","posts/61620fdc/","posts/161dc1d4/","posts/1254b2d7/","posts/84b0f900/","posts/4826c9a5/","posts/8d05fb4a/","posts/e4743e84/","posts/b8e4cbec/","posts/53120d80/","posts/394b7c6e/","posts/2238e6dd/","posts/36138da1/","posts/4114bd37/","posts/a0422dd4/","posts/28661195/","posts/f3b40e6c/","posts/9a5c3c2/","posts/22d36175/","posts/357344ce/","posts/5bbc51d7/","posts/71e17c0e/","posts/2d67944/","posts/75d149d2/","posts/da32a6cf/","posts/9bdf28fe/","posts/2579e950/","posts/c386cf1b/","posts/9cb2ece7/","posts/c6d7328c/","posts/a4cc445e/","posts/61aec8f/","posts/3f476402/","posts/283efcd9/","posts/d88e5f13/","posts/e76ad977/","posts/ebb5dc71/","posts/547d4cc2/","posts/7e8a0514/","posts/35767f2f/","posts/3fa2fffe/","posts/cd741d78/","posts/ba732dee/","posts/35d21355/","posts/8dce990f/","posts/2417b84d/","posts/87137f47/","posts/d2fdc809/","posts/de6e7ab8/","posts/e574c01a/","posts/56ee636f/","posts/a7d23c1/","posts/4f072fd7/","posts/d7f18edd/","posts/8228db57/","posts/9dcc702b/","posts/da701999/","posts/1386d0b9/","posts/4e290ddc/","posts/6d870065/","posts/c6f5a50c/","posts/b9223ce3/","posts/f4677d2b/","posts/947f8646/","posts/6d6e2c91/","posts/1a691c07/","posts/840d89a4/","posts/f30ab932/","posts/1d04d81e/","posts/6a03e888/","posts/8dbbc58f/","posts/fabcf519/","posts/9a7b7cfc/","posts/ed7c4c6a/","posts/74751dd0/","posts/3722d46/","posts/9d16b8e5/","posts/ea118873/","posts/7318d9c9/","posts/41fe95f/","posts/94a0f4ce/","posts/e3a7c458/","posts/d8f70a98/","posts/41fe5b22/","posts/3194afad/","posts/aff03a0e/","posts/c8321fba/","posts/6d427ba9/","posts/d9852e2d/","posts/621d6fb0/","posts/6bbdbdae/","posts/88d93796/","posts/a8f15a08/","posts/e26ca5c0/","posts/9b08c2f8/","posts/5d67c4fe/","posts/4cc223c0/","posts/1270e188/","posts/43ef6d99/","posts/f46e4a75/","posts/ddc70a35/","posts/793def0e/","posts/9c73b1da/","posts/f430c020/","posts/36eb6459/","posts/685b06f5/","posts/1312fdb7/","posts/78d54ae0/","posts/192c2658/","posts/28106027/","posts/50b11017/","posts/54a1572b/","posts/1ee59e86/","posts/9869bc5c/","posts/99bf1dfb/","posts/500aa71a/","posts/cbe8742c/","posts/9c648ac2/","posts/81cb4126/","posts/ff1f33/","posts/4be2c47/","posts/efe0941c/","posts/8da36f93/","posts/49e7d8d7/","posts/491beba8/","posts/7b2779ff/","posts/9ea68c78/","posts/491763c5/","posts/6a337537/","posts/ca824a92/","posts/97d4add1/","posts/a8c53633/","posts/8c27321e/","posts/39aa0631/","posts/4c4ad22e/","posts/be0b0013/","posts/7de22818/","posts/76e21c1f/","posts/dbaee672/","posts/59d9a99d/","posts/7f727ee2/","posts/38879a45/","posts/f98b7cc8/","posts/68dc7e16/","posts/626df768/","posts/2631a921/","posts/b815fba4/","posts/b4f1812a/","posts/d39c7ed7/","posts/7c7e2237/","posts/8176c029/","posts/bd4c530b/","posts/c42aeda3/","posts/ee2fccb9/","posts/3b2aed11/","posts/ed8a59d5/","posts/5db8b845/","posts/1525de29/","posts/d3e8c6d/","posts/98580ac0/","posts/f4c5f2f6/","posts/3d195c1d/","posts/1dba8d22/","posts/aa2434cf/","posts/5f429942/","posts/e0ecfe74/","posts/6ef36895/","posts/87fad4ae/","posts/4ca07e43/","posts/d247110e/","posts/f8652b18/","posts/a49b7e72/","posts/3c59b51e/","posts/95fbcd41/","posts/9b0c3626/","posts/49b6eb92/","posts/10ed3e89/","posts/b4855390/","posts/384d5ac4/","posts/6a43577e/","posts/21fe0725/","posts/e58a5d6d/","posts/edbd872e/","posts/ef14995a/","posts/976e09d8/","posts/bc902579/","posts/f7b879d4/","posts/4aab722d/","posts/d389a3ce/","posts/bcfbb93b/","posts/4e45d887/","posts/959ed6eb/","posts/8b290416/","posts/55d79eed/","posts/cf26047f/","posts/780cd25c/","posts/8e03caff/","posts/ea8048e9/","posts/64414783/","posts/73c18bdb/","posts/9234398d/","posts/f6227ced/","posts/e364f644/","posts/de86785c/","posts/7c99c9ea/","posts/ccb6788a/","posts/1ff1dcec/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true},{"name":"空梦","link":"https://kmar.top/","avatar":"https://image.kmar.top/qqavatar.jpg","descr":"有编程的烦恼？来这里说不定可以找到答案~"},{"name":"Aohui","link":"https://aohuiliu.fun","avatar":"https://www.aohuiliu.fun/images/avatar.jpeg","descr":"记录学习，记录生活。"}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
